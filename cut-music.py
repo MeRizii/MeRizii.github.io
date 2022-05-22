150186e612e161bd8aa7926aa2751500a1a22907
Karolina Olechnicka <47863699+MeRizii@users.noreply.github.com>
2022-05-23 01:53:46 +0200

    Update cut-music.py

--- cut-music.py
+++ cut-music.py
@@ -8,7 +8,7 @@ start_time = (int(start_time.split(':')[0])*60+int(start_time.split(':')[1]))*10
 stop_time = (int(stop_time.split(':')[0])*60+int(stop_time.split(':')[1]))*1000
 print("ms:",start_time,"~",stop_time)
 word = sound[start_time:stop_time]
-# save_name = "xiaye-and-anlian.mp3-"+file_name[6:]
+
 save_name = "./media/Daniel Caesar - Best Part (Audio) ft. H.E.R..mp3"
 print (save_name)
 word.export(save_name, format="mp3",tags={'artist': 'AppLeU0', 'album': save_name[:-4]})
